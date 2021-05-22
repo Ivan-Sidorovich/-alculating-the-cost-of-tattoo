5577445f22cb9d119ad1182c0377f3e7bff352a8
Ivan <sidorovich.vanya969@gmail.com>
2021-05-22 19:39:21 +0300

    ...

--- dist/index.dev.js
+++ dist/index.dev.js
@@ -1,13 +1,43 @@
 "use strict";
 
-var height, width;
+var height = 5,
+    width = 5,
+    square = height * width;
 var resultWidth = document.getElementById("result_width");
 var resultHeight = document.getElementById("result_height");
-document.getElementById("width").addEventListener("pointermove", function () {
+var result = '';
+var parameters = {
+  popUp_color: undefined,
+  popUp_pleace: undefined
+};
+
+function changingWidth() {
   resultWidth.innerHTML = this.value;
-});
-document.getElementById("height").addEventListener("pointermove", function () {
+  width = this.value;
+  square = height * this.value;
+  renderPrice();
+}
+
+function changingHeight() {
   resultHeight.innerHTML = this.value;
+  height = this.value;
+  square = this.value * width;
+  renderPrice();
+}
+
+var $height = document.getElementById("height");
+var $width = document.getElementById("width");
+$width.addEventListener("pointerdown", function () {
+  $width.addEventListener("pointermove", changingWidth);
+});
+$height.addEventListener("pointerdown", function () {
+  $height.addEventListener("pointermove", changingHeight);
+});
+$width.addEventListener("pointerup", function () {
+  $width.removeEventListener("pointermove", changingWidth);
+});
+$height.addEventListener("pointerup", function () {
+  $height.removeEventListener("pointermove", changingHeight);
 });
 var selectColor = document.getElementById("select_color");
 var selectPlece = document.getElementById("select_pleace");
@@ -15,9 +45,11 @@ var popUpColor = document.getElementById("popUp_color");
 var popUpPleace = document.getElementById("popUp_pleace");
 selectColor.addEventListener("pointerup", function () {
   popUpColor.classList.toggle('popUp_color_open');
+  document.getElementById('popUp_color_i').classList.toggle('open_i');
 });
 selectPlece.addEventListener("pointerup", function () {
   popUpPleace.classList.toggle('popUp_pleace_open');
+  document.getElementById('popUp_pleace_i').classList.toggle('open_i');
 });
 
 (function rendering() {
@@ -26,7 +58,30 @@ selectPlece.addEventListener("pointerup", function () {
     i.addEventListener("pointerup", function () {
       i.parentElement.classList.toggle("".concat(i.parentElement.id, "_open"));
       document.getElementById("".concat(i.parentElement.id, "_p")).innerHTML = i.innerHTML;
-      console.log(this.id);
+      parameters[i.parentElement.id] = this.id;
+      renderPrice();
+      document.getElementById("".concat(i.parentElement.id, "_i")).classList.toggle('open_i');
     });
   });
 })();
+
+var $span = document.getElementById("finaly_price_span");
+var $message = document.getElementById("after_result");
+
+function renderPrice() {
+  if (!parameters.popUp_color || !parameters.popUp_pleace) result = '---';else if (square <= 16) {
+    result = Math.ceil(2500 * parameters.popUp_color / 50) * 50;
+    $message.style.display = 'block';
+  } else {
+    result = Math.ceil(((square - 16) * parameters.popUp_pleace + 2500) * parameters.popUp_color / 50) * 50;
+    $message.style.display = 'block';
+  }
+  $span.innerHTML = result;
+}
+
+;
+renderPrice();
+document.getElementById('detailed_p').addEventListener("pointerup", function () {
+  document.getElementById('detailed_i').classList.toggle('fa-plus');
+  document.getElementById('rules').classList.toggle('open_rules');
+});
--- index.js
+++ index.js
@@ -1,16 +1,55 @@
-let height, width;
+let height = 5,
+    width = 5,
+    square = height * width;
 
 let resultWidth = document.getElementById("result_width");
 let resultHeight = document.getElementById("result_height");
 
-document.getElementById("width").addEventListener("pointermove", function () {
+let result = '';
+
+let parameters = {
+    popUp_color: undefined,
+    popUp_pleace: undefined
+}
+
+function changingWidth() {
     resultWidth.innerHTML = this.value;
+    width = this.value;
+    square = height * this.value;
+    renderPrice();
+}
 
-})
-
-document.getElementById("height").addEventListener("pointermove", function () {
+function changingHeight() {
     resultHeight.innerHTML = this.value;
-})
+    height = this.value;
+    square = this.value * width;
+    renderPrice();
+}
+
+let $height = document.getElementById("height");
+let $width = document.getElementById("width");
+
+
+
+$width.addEventListener("pointerdown", function () {
+    $width.addEventListener("pointermove", changingWidth);
+});
+
+$height.addEventListener("pointerdown", function () {
+    $height.addEventListener("pointermove", changingHeight);
+});
+
+
+
+$width.addEventListener("pointerup", function () {
+    $width.removeEventListener("pointermove", changingWidth);
+});
+
+$height.addEventListener("pointerup", function () {
+    $height.removeEventListener("pointermove", changingHeight);
+});
+
+
 
 let selectColor = document.getElementById("select_color");
 let selectPlece = document.getElementById("select_pleace");
@@ -20,10 +59,12 @@ let popUpPleace = document.getElementById("popUp_pleace");
 
 selectColor.addEventListener("pointerup", function () {
     popUpColor.classList.toggle('popUp_color_open');
+    document.getElementById('popUp_color_i').classList.toggle('open_i');
 });
 
 selectPlece.addEventListener("pointerup", function () {
     popUpPleace.classList.toggle('popUp_pleace_open');
+    document.getElementById('popUp_pleace_i').classList.toggle('open_i');
 });
 
 
@@ -34,7 +75,34 @@ selectPlece.addEventListener("pointerup", function () {
         i.addEventListener("pointerup", function () {
             i.parentElement.classList.toggle(`${i.parentElement.id}_open`);
             document.getElementById(`${i.parentElement.id}_p`).innerHTML = i.innerHTML;
-            console.log(this.id);
+            parameters[i.parentElement.id] = this.id;
+            renderPrice();
+            document.getElementById(`${i.parentElement.id}_i`).classList.toggle('open_i');
         });
     })
 })();
+
+let $span = document.getElementById("finaly_price_span");
+let $message = document.getElementById("after_result");
+
+function renderPrice() {
+    if (!parameters.popUp_color || !parameters.popUp_pleace) result = '---';
+    else if (square <= 16) {
+        result = Math.ceil((2500 * parameters.popUp_color) / 50) * 50;
+        $message.style.display = 'block';
+    } else {
+        result = Math.ceil(
+            (((square - 16) * parameters.popUp_pleace + 2500) * parameters.popUp_color) / 50
+        ) * 50;
+        $message.style.display = 'block';
+    }
+    $span.innerHTML = result;
+};
+renderPrice();
+
+
+document.getElementById('detailed_p').addEventListener("pointerup", function () {
+    document.getElementById('detailed_i').classList.toggle('fa-plus');
+    document.getElementById('rules').classList.toggle('open_rules');
+
+})
